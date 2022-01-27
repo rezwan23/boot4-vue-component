6023bf603ccf5594915b7575ff14211b418cd463
rezwan23 <razwanulghani@gmail.com>
2022-01-27 17:37:17 +0600

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import Bootstrap4 from '@/bootstrap4.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Bootstrap4
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <bootstrap4 />
+  </div>
+</template>
---
+++ src/bootstrap4.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span v-if="!content">
+        <center class="text-danger"><h1>No Content</h1></center>
+    </span>
+    <span v-html="content" v-else></span>
+  </div>
+</template>
+
+<script>
+require('bootstrap')
+export default {
+    props:['content'],
+  name: "GhaniBootstrap4Component",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url(https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css);
+</style>
